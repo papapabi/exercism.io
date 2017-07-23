36727d31481a65a848bb6c4bf9992901ebb17427
Joshua Pabilona <pabilona.joshua@gmail.com>
2017-07-23 14:39:13 +0800

    ruby: refactor 'Hamming'

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,9 +1,15 @@
 class Hamming
-  def self.compute(dna_string, other_dna_string)
-    hamming_difference = 0
-    dna_string.chars.zip(other_dna_string.chars).each do |c, o_c|
-      hamming_difference += 1 if c != o_c
+  class << self
+    def compute(dna_string, other_dna_string)
+      unless dna_string.length == other_dna_string.length
+        raise ArgumentError.new('Strings must be of equal length')
+      end
+      dna_string.chars.zip(other_dna_string.chars).count { |c, o_c| c != o_c }
     end
-    hamming_difference
   end
 end
+
+module BookKeeping
+  VERSION = 3
+end
+

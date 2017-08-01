c20852c50e3f3f676bc049a8638e74592b2f78ad
Joshua Pabilona <pabilona.joshua@gmail.com>
2017-08-02 06:33:42 +0800

    ruby: refactor 'Difference Of Squares'

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,16 +1,17 @@
 class Squares
+  attr_accessor :square_of_sum, :sum_of_squares
+
   def initialize(n)
     @n = n 
     @sum_of_squares = (1..n).map { |i| i**2 }.reduce(:+)
     @square_of_sum = (1..n).reduce(:+)**2
   end  
-  def square_of_sum
-    @square_of_sum
-  end
-  def sum_of_squares
-    @sum_of_squares
-  end
+
   def difference
-    @square_of_sum - @sum_of_squares
+    square_of_sum - sum_of_squares
   end
+end
+
+module BookKeeping
+  VERSION = 4
 end
--- ruby/difference-of-squares/difference_of_squares_test.rb
+++ ruby/difference-of-squares/difference_of_squares_test.rb
@@ -66,7 +66,7 @@ class DifferenceOfSquaresTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
+    # skip
     assert_equal 4, BookKeeping::VERSION
   end
 end

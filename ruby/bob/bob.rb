4f1252cd742026a923178ff10ec0f7e2f90286b0
Joshua Pabilona <pabilona.joshua@gmail.com>
2017-08-02 06:24:06 +0800

    ruby: solve 'Bob'

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -6,19 +6,25 @@ class Bob
 
   def self.hey(remark)
     normalized_remark = normalize(remark)
-    if remark.end_with?("?")
-      RESPONSES[:question]
-    elsif remark.strip.empty?
+    if normalized_remark.empty?
       RESPONSES[:silence]
-    elsif remark.upcase == remark && remark.downcase != remark
+    elsif yelling?(normalized_remark)
       RESPONSES[:yelling]
+    elsif normalized_remark.end_with?("?")
+      RESPONSES[:question]
     else
       RESPONSES[:default]
     end
   end
 
   private
+
   def self.normalize(remark)
+    remark.gsub(/\s+/, ' ').strip
+  end
+
+  def self.yelling?(remark)
+    remark.upcase == remark && remark.downcase != remark
   end
 end
 

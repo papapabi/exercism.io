9df35bb5bffe6752a1b507aef5f60d1d86bd4b25
Joshua Pabilona <pabilona.joshua@gmail.com>
2017-08-17 11:14:28 +0800

    ruby: refactor 'Prime Factors' -- change #each to #map

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -2,11 +2,9 @@ require 'prime'
 
 class PrimeFactors
   def self.for(n)
-    factors = []
-    return factors if n == 1
-    n.prime_division.each do |(prime, exponent)|
-      exponent.times { factors << prime }
-    end
-    factors
+    return [] if n == 1
+    n.prime_division.map do |(prime, exponent)|
+      [].tap { |tap| exponent.times { tap << prime } }
+    end.flatten
   end
 end

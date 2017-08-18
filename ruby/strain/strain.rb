f2c608507b80f1b9bb53c2e92147e091b2df4f8a
Joshua Pabilona <pabilona.joshua@gmail.com>
2017-08-18 17:15:12 +0800

    ruby: final 'Strain' refactoring

--- ruby/strain/strain.rb
+++ ruby/strain/strain.rb
@@ -2,12 +2,16 @@ module Enumerable
   def keep(&block)
     if block_given?
       [].tap { |result| each { |e| yield(e) ? result << e : nil } }
+    else 
+      enum_for(:keep) { size }
     end
   end
 
   def discard(&block)
     if block_given?
       [].tap { |result| each { |e| yield(e) ? nil : result << e } }
+    else 
+      enum_for(:discard) { size }
     end
   end
 end

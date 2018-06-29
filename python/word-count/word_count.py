e1479c557c3443f0caee67b353a47f5326ab7851
Joshua Pabilona <pabilona.joshua@gmail.com>
2018-06-29 17:58:19 +0800

    python: solve 'Word Count'

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,10 +1,9 @@
 import re
+from collections import Counter
 
-word_regex = re.compile(r"(?<![@#])\b[A-Za-z0-9]+(?:'[A-Za-z0-9]+)?")
+word_regex = re.compile(r"(?<![@#])[A-Za-z0-9]+(?:'[A-Za-z0-9]+)?")
 
 def word_count(phrase):
-    occurrences = {}
     words = re.findall(word_regex, phrase.lower())
-    for word in words:
-        occurrences[word] = occurrences.get(word, 0) + 1
+    occurrences = Counter(words)
     return occurrences

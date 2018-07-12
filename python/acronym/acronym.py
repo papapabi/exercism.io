9dc29d946deec4fc1915d0f02f2f36cf1b01ef52
Joshua Pabilona <pabilona.joshua@gmail.com>
2018-07-13 07:56:54 +0800

    python: refactor 'Acronym'

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,7 +1,8 @@
 import re
 
 def abbreviate(words):
-    word_regex = re.compile(r'\b[0-9a-zA-Z]+')
-    normalized_words = re.findall(word_regex, words)
-    first_letters = (w[0] for w in normalized_words)
-    return ''.join(first_letters).upper()
+    return ''.join(_first_letters(words)).upper()
+
+def _first_letters(words):
+    first_letters = re.compile(r'\b[0-9a-zA-Z]')
+    return re.findall(first_letters, words)

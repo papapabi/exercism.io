855f0a983d343c7c1fdeeee0bda586cfaa9b43f8
Joshua Pabilona <pabilona.joshua@gmail.com>
2018-07-19 12:56:03 +0800

    python: rational-numbers/ ; impl all ops except exponentiation

--- python/rational-numbers/rational_numbers.py
+++ python/rational-numbers/rational_numbers.py
@@ -1,12 +1,24 @@
 from math import gcd
 
+
+def _simplest_form(numer, denom):
+    """Returns the simplest form of a numer and denom, in a 2-tuple"""
+    _gcd = gcd(numer, denom)
+    if numer * denom > 0:
+        numer = abs(numer)
+        denom = abs(denom)
+    else:
+        numer = -abs(numer)
+        denom = abs(denom)
+    return (numer // _gcd, denom // _gcd)
+
+
 class Rational(object):
+
     def __init__(self, numer, denom):
         if denom == 0:
             raise ValueError("denom cannot be 0")
-        _gcd = gcd(numer, denom)
-        self.numer = numer / _gcd
-        self.denom = denom / _gcd
+        self.numer, self.denom  = _simplest_form(numer, denom)
 
     def __eq__(self, other):
         return self.numer == other.numer and self.denom == other.denom
@@ -15,19 +27,29 @@ class Rational(object):
         return '{}/{}r'.format(self.numer, self.denom)
 
     def __add__(self, other):
-        pass
+        numer = self.numer * other.denom + other.numer * self.denom
+        denom = self.denom * other.denom
+        return Rational(numer, denom)
 
     def __sub__(self, other):
-        pass
+        numer = self.numer * other.denom - other.numer * self.denom
+        denom = self.denom * other.denom
+        return Rational(numer, denom)
 
     def __mul__(self, other):
-        pass
+        numer = self.numer * other.numer
+        denom = self.denom * other.denom
+        return Rational(numer, denom)
 
     def __truediv__(self, other):
-        pass
+        numer = self.numer * other.denom
+        denom = self.denom * other.numer
+        return Rational(numer, denom)
 
     def __abs__(self):
-        pass
+        abs_numer = abs(self.numer)
+        abs_denom = abs(self.denom)
+        return Rational(abs_numer, abs_denom)
 
     def __pow__(self, power):
         pass

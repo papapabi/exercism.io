3b3c0c9a67575df19793119a26bfa12025410afd
Joshua Pabilona <pabilona.joshua@gmail.com>
2018-07-23 16:45:26 +0800

    python: refactor 'Triangle'

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,18 +1,23 @@
-def is_equilateral(sides):
-    return len(set(sides)) == 1 and is_triangle(sides) \
-            and not _has_zero_sides(sides)
-
-def is_isosceles(sides):
-    return 1 <= len(set(sides)) <= 2 and is_triangle(sides) \
-            and not _has_zero_sides(sides)
-
-def is_scalene(sides):
-    return len(set(sides)) == 3 and is_triangle(sides) \
-            and not _has_zero_sides(sides)
-
-def _has_zero_sides(sides):
-    return 0 in sides
+def has_no_zero_sides(sides):
+    return 0 not in sides
 
 def is_triangle(sides):
     a, b, c = sides
     return a + b >= c and b + c >= a and a + c >= b
+
+def valid_triangle(f):
+    def inner(sides):
+        return f(sides) and is_triangle(sides) and has_no_zero_sides(sides)
+    return inner
+
+@valid_triangle
+def is_equilateral(sides):
+    return len(set(sides)) == 1 
+
+@valid_triangle
+def is_isosceles(sides):
+    return 1 <= len(set(sides)) <= 2
+
+@valid_triangle
+def is_scalene(sides):
+    return len(set(sides)) == 3 

49610e646063839afb17f58bcd65ac4e92072f6a
Joshua Pabilona <pabilona.joshua@gmail.com>
2018-07-24 19:14:34 +0800

    python: refactor rna-transcription/

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,13 +1,16 @@
 import re
 
-complement = {'G': 'C',
-              'C': 'G',
-              'T': 'A',
-              'A': 'U'}
+DNA_TO_RNA_MAPPING = str.maketrans('GCTA', 'CGAU')
 
 def to_rna(dna):
-    p = re.compile('[^GCTA]')
-    if p.search(dna):
-        return ''
-    else:
-        return ''.join(complement[nucleotide] for nucleotide in dna)
+    """Returns the RNA complement of a given DNA strand.
+
+    :param dna (str): the DNA strand.
+    :returns: the RNA complement.
+    :raises ValueError: if the argument dna contains characters other than
+    [GCTA]
+    """
+    not_dna_chars = re.compile(r'[^GCTA]|^$')
+    if not_dna_chars.search(dna):
+        raise ValueError(f'cannot contain characters other than [GCTA]')
+    return dna.translate(DNA_TO_RNA_MAPPING)
--- python/rna-transcription/rna_transcription_test.py
+++ python/rna-transcription/rna_transcription_test.py
@@ -3,14 +3,15 @@ import unittest
 from rna_transcription import to_rna
 
 
-# test cases adapted from `x-common//canonical-data.json` @ version: 1.0.0
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.2.0
 
-class DNATests(unittest.TestCase):
-    def test_transcribes_guanine_to_cytosine(self):
-        self.assertEqual(to_rna('G'), 'C')
+class RnaTranscriptionTest(unittest.TestCase):
 
     def test_transcribes_cytosine_to_guanine(self):
         self.assertEqual(to_rna('C'), 'G')
+
+    def test_transcribes_guanine_to_cytosine(self):
+        self.assertEqual(to_rna('G'), 'C')
 
     def test_transcribes_thymine_to_adenine(self):
         self.assertEqual(to_rna('T'), 'A')
@@ -18,17 +19,39 @@ class DNATests(unittest.TestCase):
     def test_transcribes_adenine_to_uracil(self):
         self.assertEqual(to_rna('A'), 'U')
 
-    def test_transcribes_all_occurences(self):
-        self.assertMultiLineEqual(to_rna('ACGTGGTCTTAA'), 'UGCACCAGAAUU')
+    def test_transcribes_all_occurrences(self):
+        self.assertEqual(to_rna('ACGTGGTCTTAA'), 'UGCACCAGAAUU')
 
-    def test_correctly_handles_single_invalid_input(self):
-        self.assertEqual(to_rna('U'), '')
+    def test_does_not_transcribe_empty_string(self):
+        with self.assertRaisesWithMessage(ValueError):
+            to_rna('')
 
-    def test_correctly_handles_completely_invalid_input(self):
-        self.assertMultiLineEqual(to_rna('XXX'), '')
+    def test_does_not_transcribe_blank_string(self):
+        with self.assertRaisesWithMessage(ValueError):
+            to_rna('            ')
 
-    def test_correctly_handles_partially_invalid_input(self):
-        self.assertMultiLineEqual(to_rna('ACGTXXXCTTAA'), '')
+    def test_does_not_transcribe_if_dna_contains_invalids(self):
+        with self.assertRaisesWithMessage(ValueError):
+            to_rna('XCTGPABIXXYQ')
+
+    def test_does_not_transcribe_if_dna_is_broken_even_with_only_valids(self):
+        with self.assertRaisesWithMessage(ValueError):
+            to_rna('GTCA    GTCA')
+
+    def test_does_not_transcribe_if_dna_has_trailing_whitespace(self):
+        with self.assertRaisesWithMessage(ValueError):
+            to_rna("""GCTA            
+                   """)
+
+    # Utility functions
+    def setUp(self):
+        try:
+            self.assertRaisesRegex
+        except AttributeError:
+            self.assertRaisesRegex = self.assertRaisesRegexp
+
+    def assertRaisesWithMessage(self, exception):
+        return self.assertRaisesRegex(exception, r".+")
 
 
 if __name__ == '__main__':

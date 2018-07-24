b863f8be96cc9fc4474769bc558b6c824017369f
Joshua Pabilona <pabilona.joshua@gmail.com>
2018-07-24 19:23:52 +0800

    python: refactor hamming/

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,9 +1,10 @@
 def distance(strand_a, strand_b):
+    """Returns the hamming distance between two DNA strands.
+    
+    :param strand_a (str): the first DNA string.
+    :param strand_b (str): the second DNA string.
+    :returns: the hamming distance.
+    """
     if len(strand_a) != len(strand_b):
         raise ValueError("strings are of unequal length")
-    distance = 0
-    zipped_strands = zip(strand_a, strand_b)
-    for a, b in zipped_strands:
-        if a != b: 
-            distance += 1
-    return distance
+    return sum(1 for a, b in zip(strand_a, strand_b) if a != b) 

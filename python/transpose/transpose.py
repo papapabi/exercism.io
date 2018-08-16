cca3292d21c5b9fde1eb32f676f29cb45c2f923d
Joshua Pabilona <pabilona.joshua@gmail.com>
2018-08-16 23:24:51 +0800

    python: solve 'Transpose'

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -1,7 +1,8 @@
 from itertools import zip_longest
 
+
 def transpose(input_lines):
     lines = input_lines.split('\n')
-    transposed_lines = (''.join(aggregate) 
-                        for aggregate in zip_longest(*lines, fillvalue=' '))
+    transposed_lines = (''.join(aggregate).rstrip('&').replace('&', ' ')
+                        for aggregate in zip_longest(*lines, fillvalue='&'))
     return '\n'.join(transposed_lines)

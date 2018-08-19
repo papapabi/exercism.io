8c73812bbd4c04c0c1c2e3f5b54af70df5aab141
Joshua Pabilona <pabilona.joshua@gmail.com>
2018-08-19 15:38:42 +0800

    python: refactor isbn-verifier/

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,29 +1,26 @@
 import re
 
+
+ISBN_REGEX = re.compile('[0-9]{9}[0-9Xx]')
+
+
 def verify(isbn):
-    normalized_isbn = normalize(isbn)
-    isbn_length = len(normalized_isbn)
-    if isbn_length != 10:
+    isbn = isbn.replace('-', '') # Remove hyphens from the string, if it has.
+    if len(isbn) != 10:
         return False
-    if not is_valid_isbn_characters(normalized_isbn):
+    if not valid_isbn_characters(isbn):
         return False
-    multiplicand = 10
-    value = 0
-    normalized_isbn_integers = [int(x) if x.isdigit() else 10 for x in
-                                normalized_isbn]
-    for integer in normalized_isbn_integers:
-        value += integer*multiplicand
-        multiplicand -= 1
 
-    return (value % 11 == 0)
+    # Get product up to (but not including) the last char in the ISBN string.
+    products = [int(isbn_digit) * multiplier for isbn_digit, multiplier
+                in zip(isbn[:-1], range(10, 0, -1))]
 
-def normalize(isbn):
-    """Remove dashes from the string, if it has.
-    """
-    return isbn.replace("-", "")
+    # The last char is either a digit or X, append the appropriate value.
+    products.append(int(isbn[-1]) if isbn[-1].isdigit() else 10)
 
-def is_valid_isbn_characters(isbn):
-    """Check if given string matches the isbn pattern.
-    """
-    regex = re.compile('[0-9]{9}[0-9X]')
-    return bool(regex.search(isbn))
+    return sum(products) % 11 == 0
+
+
+def valid_isbn_characters(isbn):
+    """Returns true if the given string matches the ISBN-10 format."""
+    return ISBN_REGEX.match(isbn)

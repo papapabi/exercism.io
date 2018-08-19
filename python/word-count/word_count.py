b987b58f024d6c87e9fbf9e464301e57ed274230
Joshua Pabilona <pabilona.joshua@gmail.com>
2018-08-19 21:01:49 +0800

    python: refactor word-count/

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,9 +1,8 @@
 import re
 from collections import Counter
 
-word_regex = re.compile(r"(?<![@#])[A-Za-z0-9]+(?:'[A-Za-z0-9]+)?")
 
 def word_count(phrase):
-    words = re.findall(word_regex, phrase.lower())
-    occurrences = Counter(words)
-    return occurrences
+    words = [word.strip("'") for word in
+             re.findall(r'[a-z\d]*\'?[a-z\d]+', phrase.lower())]
+    return Counter(words)

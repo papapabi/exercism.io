46a8c144063c71444b840ae26605f8ebf753dc09
Joshua Pabilona <pabilona.joshua@gmail.com>
2018-08-19 16:21:57 +0800

    python: refactor isogram/

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,9 +1,12 @@
-def is_isogram(string):
-    orig = list(string.lower())
-    for letter in orig:
-        if letter.isalpha():
-            if orig.count(letter) > 1:
-                return False
-            else:
-                continue
-    return True
+import re
+from collections import Counter
+
+
+def is_isogram(s):
+    if not s:
+        return True # The empty string is an isogram by definition.
+    s = s.lower()
+    s = re.sub(r'[^A-Za-z]', '', s) # Remove all non-alphabetic characters.
+    # counter.most_common([n]) returns a list of 2-tuples of the elements and
+    # their corresponding counts.
+    return Counter(s).most_common(1)[0][1] == 1

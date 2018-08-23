be0f62e28dee57d3ff99a3a19c4c6b3362829732
Joshua Pabilona <pabilona.joshua@gmail.com>
2018-08-23 09:40:55 +0800

    python: solve 'All Your Base'

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -2,29 +2,37 @@ from math import log, floor
 
 
 def rebase(input_base, digits, output_base):
-    if output_base < 2:
-        raise ValueError("output_base must be greater than 2")
-    base_10_digits = _to_base10(input_base, digits)
-    if output_base == 10:
-        return base_10_digits
+    if input_base < 2 or output_base < 2:
+        raise ValueError('bases must be greater than 2')
+    if not _valid_digits(input_base, digits):
+        raise ValueError(
+            f'digits contains element not in [0, {input_base}): ' \
+            f'digits = {digits}')
 
-    base_10 = sum(base_10_digits)
-    highest_output_base_exp = floor(log(base_10, output_base))
     output_base_digits = []
-    
+    if not digits or all(d == 0 for d in digits):
+        return output_base_digits
+
+    base_10 = sum(_to_lc(input_base, digits)) # The given number in base 10.
+    highest_output_base_exp = floor(log(base_10, output_base))
+
     for exp in range(highest_output_base_exp, -1, -1):
-        intermediary = output_base ** exp
-        # if intermediary > base_10, append 0 and continue
-        if intermediary > base_10:
+        magnitude = output_base ** exp
+        if magnitude > base_10:
             output_base_digits.append(0)
             continue
-        output_base_digits.append(base_10 // intermediary)
-        base_10 -= intermediary
+        digit = base_10 // magnitude
+        output_base_digits.append(digit)
+        base_10 -= digit * magnitude
 
     return output_base_digits
 
 
-def _to_base10(input_base, digits):
-    """Return a list of base 10 digits."""
+def _to_lc(input_base, digits):
+    """Return the linear combination of the powers of input_base and digits."""
     return [d * input_base**exp 
             for d, exp in zip(digits, reversed(range(len(digits))))]
+
+def _valid_digits(input_base, digits):
+    """Return true if all digits are in the interval [0, input_base)."""
+    return all(digit in range(0, input_base) for digit in digits)

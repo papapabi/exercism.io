c699cf49d682221e59da48b3b4d16b7b850d1068
Joshua Pabilona <pabilona.joshua@gmail.com>
2018-08-31 19:10:52 +0800

    python: init poker/

---
+++ python/poker/poker.py
@@ -0,0 +1,12 @@
+# There are 9 hand-ranking categories when using a standard 52-card deck.
+# Use a histogram-based solution?
+# Suits are apparently not ranked in poker.
+# from collections import Counter
+class Card(object):
+    def __init__(self, rank, suit):
+        # Arranged from lowest to highest.
+        ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+        suit = 'spades diam
+
+def best_hands(hands):
+    pass
